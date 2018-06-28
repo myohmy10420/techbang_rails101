d0a0b2678b1dd5805d2bd00ae2c230cb2cebc65b
Kurt <kurt@techbangdeMac-mini.local>
2018-06-28 15:42:37 +0800

    Implement Groups CRUD

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,51 @@
+class GroupsController < ApplicationController
+  before_action :find_group, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group.destroy
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def find_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+end
