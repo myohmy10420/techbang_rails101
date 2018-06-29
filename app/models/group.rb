67d55a080ea225da37af748c0f11ccb0d423f155
Kurt Hsu <myohmy10420@gmail.com>
2018-06-29 10:59:43 +0800

    associate groups blongs to user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,10 @@
 class Group < ActiveRecord::Base
   has_many :posts
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   validates :title, presence: true
+
+  def editable_by?(user)
+    user && user == owner
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180628085120) do
+ActiveRecord::Schema.define(version: 20180629025540) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
