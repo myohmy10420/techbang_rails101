9e97d508a63ec861a4f3c26d75d8cfe08a39dbe7
Kurt Hsu <myohmy10420@gmail.com>
2018-06-29 10:12:30 +0800

    Custmer global flash in help

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def flash_messages
+    flash_types = { success: "success", error: "danger", alert: "warning", notice: "info" }
+
+    alerts =
+      flash.map do |msg_type, message|
+        alert_content = message
+        flash_type    = flash_types[msg_type.to_sym] || msg_type
+        alert_class   = "alert alert-#{flash_type} alert-dismissible"
+
+        content_tag(:div, alert_content, class: alert_class, role: 'alert')
+      end
+
+    alerts.join("\n").html_safe
+  end
 end
