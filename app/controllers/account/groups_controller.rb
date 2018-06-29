e0eb756a755058eff8f0ac70855649a187ae8500
Kurt Hsu <myohmy10420@gmail.com>
2018-06-29 17:31:32 +0800

    Groups and posts list order by updated_at DESC

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,6 +2,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.participated_groups
+    @groups = current_user.participated_groups.order("updated_at DESC")
   end
 end
