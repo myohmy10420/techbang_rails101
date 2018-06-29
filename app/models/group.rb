39d30c3cd656014731c0e304c97cbc99629d46e0
Kurt Hsu <myohmy10420@gmail.com>
2018-06-29 15:07:04 +0800

    Assciate users and groups many_to_many by group_users

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ActiveRecord::Base
   has_many :posts
+
+  has_many :group_users
+  has_many :members, through: :group_users, source: :user
+
   belongs_to :owner, class_name: "User", foreign_key: :user_id
 
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :groups
   has_many :posts
 
+  has_many :group_users
+  has_many :participated_groups, through: :group_users, source: :group
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
