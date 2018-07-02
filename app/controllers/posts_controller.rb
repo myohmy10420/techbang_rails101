482e293c5d31b1b02f65ea72ac67442c381855e0
Kurt Hsu <myohmy10420@gmail.com>
2018-07-02 15:27:01 +0800

    Only member of this group can new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group
   before_action :find_post, only: [:edit, :update, :destroy]
+  before_action :member_required, :only => [:new, :create]
 
   def new
     @post = @group.posts.build
@@ -22,7 +23,6 @@ class PostsController < ApplicationController
   end
 
   def update
-
     if @post.update(posts_params)
       redirect_to group_path(@group)
     else
@@ -31,7 +31,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
     @post.destroy
 
     redirect_to group_path(@group)
@@ -51,4 +50,10 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
 
+  def member_required
+    if !current_user.is_member_of?(@group)
+      flash[:warning] = " You are not member of this group!"
+      redirect_to group_path(@group)
+    end
+  end
 end
