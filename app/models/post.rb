69258141835454e6ee71627bef6fd975d5a71351
Kurt Hsu <myohmy10420@gmail.com>
2018-07-02 19:06:37 +0800

    Groups list of backstage order by Post Count

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  belongs_to :group
+  belongs_to :group, counter_cache: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
 
   validates :content, presence: true
